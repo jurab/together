a9e8427179476e79bc685fc157edff9d665f49e5
jurab <brazdil94@gmail.com>
2022-11-21 16:02:02 +0100

    Revert cleanup

--- together/chats/schema.py
+++ together/chats/schema.py
@@ -1,9 +1,7 @@
 
-import channels
-import channels_graphql_ws
 import graphene
 
-from api.fields import NestedField, ReverseField, ModelField
+from api.fields import NestedField, ReverseField
 from api.filters import DjangoFilter, PaginationFilter, IDFilter
 from api.mutations import Save
 from api.registry import register_type, register_mutation
@@ -51,3 +49,20 @@ class MessageType:
             NestedField('sender', UserType),
             ReverseField(ChatType, 'messages'),
         }
+
+
+@register_mutation
+class SendChatMessage(Save):
+    class Meta:
+        model = Message
+        exclude_arguments = 'id created modified'.split()
+
+    @classmethod
+    def mutate(cls, root, info, chat, sender, text):
+        """Mutation "resolver" - store and broadcast a message."""
+
+        chat = Chat.objects.get(id=chat)
+        sender = User.objects.get(id=sender)
+        message = Message.objects.create(chat=chat, sender=sender, text=text)
+
+        return cls(message=message)
